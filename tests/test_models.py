e1e518a09d0188e0ba9f3c4b19223e2f59cd6159
Daniel Nunes <daniel.nunes@talkdesk.com>
2020-10-05 03:54:23 +0100

    Add tests to models

---
+++ tests/test_models.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+
+from app.models import Author, Occurrence
+
+
+class AuthorModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        user = User.objects.create(username='author')
+        Author.objects.create(user=user)
+
+    def test_author_has_username_field_from_user(self):
+        author = Author.objects.get(user_id=1)
+        self.assertEquals('author', author.user.username)
+
+
+class OccurrenceModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        user = User.objects.create(username='author')
+        author = Author.objects.create(user=user)
+        point = 'POINT(0 0)'
+        description = 'description'
+        category = 'construction'
+        Occurrence.objects.create(author=author, point=point, description=description, category=category)
+
+    def test_default_status(self):
+        occurrence = Occurrence.objects.get(id=1)
+        self.assertEquals('to_validate', occurrence.status)
--- tests/tests.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-
-
-# Create your tests here.
-class Dummy(TestCase):
-    def test_x(self):
-        self.assertTrue(True)
